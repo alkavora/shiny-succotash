a9ebfae8d9a592b3548181607855732f71a9cf11
HitanshShah <31037909+HitanshShah@users.noreply.github.com>
2021-10-01 19:19:17 +0530

    Update leadersRouter.js

--- leadersRouter.js
+++ leadersRouter.js
@@ -49,4 +49,27 @@ leadersRouter
 		res.end(`Deleted ${JSON.stringify(req.params)} from leaders`);
 	});
 
+leadersRouter
+	.route('/:csikjsce')
+	.get((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Getting Leader #${JSON.stringify(req.params)}`);
+	})
+	.post((req, res, next) => {
+		res.statusCode = 403;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Forbidden Request | Cannot POST on this endpoint`);
+	})
+	.put((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Updated Leader #${JSON.stringify(req.params)} `);
+	})
+	.delete((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`Deleted ${JSON.stringify(req.params)} from leaders`);
+	});
+
 module.exports = leadersRouter;
